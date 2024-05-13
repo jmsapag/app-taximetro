b8a1ce21ac0ca3a7418fbf88c54912f2b23b2f73
jmsapag <jmsapag17@gmail.com>
2024-05-13 14:19:23 -0300

    nuevo front + server

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,74 @@
 
+// SERVER/CONTROLLER
+
 const express = require('express')
 const app = express()
 const port = 4000
 
 require("dotenv").config();
 
-const registerRoutes = require('./routes/registers')
-
-app.get("/api", (req, res) => {
-    res.json({"users": ["one", "two"]})
-})
-
-app.get("/mqttConnDetails", (req, res) => {
-    res.send(
-        JSON.stringify({
-            mqttServer: process.env.MQTT_BROKER,
-            mqttTopic: process.env.MQTT_TOPIC,
-        })
-    );
+app.get("/registers", async (req, res) => {
+    try {
+        const message = await publishMessage();
+        res.send(message);
+    } catch (error) {
+        console.error("Error al publicar o recibir el mensaje:", error);
+        res.status(500).json({ error: "Error al obtener el mensaje" });
+    }
 });
 
-// middleware
-app.use(express.json())
-
-app.use( (req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
-
-// routes
-app.use('/api/registers', registerRoutes)
-
 app.listen(port, () => { console.log(`Example app listening on port ${port}`); })
+
+
+// SERVICE
+
+const mqtt = require("mqtt");
+const config = require("./config");
+const mqttUri = 'mqtt://' + config.mqtt.hostname + ':' + config.mqtt.port;
+const appClient = mqtt.connect(mqttUri);
+
+// subscribe to a topic
+appClient.on("connect", () => {
+    appClient.subscribe(['bd/res/register', 'esp/res/emergency'], (err) => {
+        if (!err) {
+            console.log("App client connected");
+        }
+    });
+});
+
+// handle received messages from the subscribed topics
+appClient.on('message', function (topic, message) {
+    // make something depending on the topic, emergency case and get all register
+    switch (topic) {
+        case 'bd/res/register':
+            break;
+        case 'esp/res/emergency':
+            app.get("/emergency", (req, res) => {
+                res.send(message)
+            })
+            break;
+        default:
+            console.log('Topic no reconocido');
+    }
+});
+
+// SERVICE
+
+function publishMessage() {
+    return new Promise((resolve, reject) => {
+        console.log("Publicando mensaje...");
+        appClient.publish('bd/req/register', "hola");
+        console.log("Mensaje publicado");
+
+        appClient.on("message", function (topic, message) {
+            console.log("Mensaje recibido en el tópico:", topic);
+            if (topic === 'bd/res/register') {
+                console.log("Respuesta recibida:", message.toString());
+                resolve(JSON.parse(message.toString()));
+            }
+        });
+    });
+}
+
+
+module.exports = { appClient, publishMessage };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,61 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
-import {useNavigate, Routes, Route, Navigate} from "react-router-dom";
-import Home from "./Component/Home";
-import MQTTComponent from "./Component/MQTTComponent";
+import icono from './assets/car.png';
 
 function App() {
 
-  return (
-      <Routes>
+    const [registers, setRegisters] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/mqtt" element={<MQTTComponent/>}/>
+    useEffect(() => {
+        fetch("/registers")
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Error al obtener los registros");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setRegisters(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                setError(error.message);
+                setLoading(false);
+            });
+    }, []);
 
-      </Routes>
-  );
+    return (
+        <div>
+
+            <div className="header">
+                <div className="icono-titulo">
+                    <div className="icono-app">
+                        <img src={icono} alt="Icono de la aplicación" width={70}/>
+                    </div>
+                    <div className="titulo">
+                        <h1>CityHopper</h1>
+                    </div>
+                </div>
+            </div>
+
+            <br/>
+
+            <div className="registers">
+                <h2>Historial de Registros</h2>
+                {loading && <p>Loading...</p>}
+                {error && <p>Error: {error}</p>}
+                {!loading && !error && (
+                    <ul>
+                        {registers.map((register, index) => (
+                            <li key={index}>{register}</li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
